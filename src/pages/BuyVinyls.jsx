e88eb4e5e41b920f63f20de57f9a5634b1023a93
Steven <stevenmaldonado10@hotmail.com>
2022-05-27 11:06:47 -0500

    update BuyVinyls page

--- src/pages/BuyVinyls.jsx
+++ src/pages/BuyVinyls.jsx
@@ -73,24 +73,30 @@ function BuyVinyls() {
           {
             items.map((item, index)=>{
               return(
-                <tr key={index}>
-                  <td>
-                    <img src={item.image} style={{height:'6rem'}} />
-                  </td>
-                  <td>{item.album}</td>
-                  <td>{item.artist}</td>
-                  <td>${item.price}</td>
-                  <td>Quantity ({item.quantity})</td>
-                  <td>Total: ${item.price * item.quantity}</td>
-                  <td> 
-                    <button className='btn btn-info ms-2' 
-                    onClick={()=>updateItemQuantity(item.id, item.quantity-1)}>-</button>
-                    <button className='btn btn-info ms-2'  
-                    onClick={()=>updateItemQuantity(item.id, item.quantity+1)}>+</button>
-                    <button className='btn btn-danger ms-2' 
-                     onClick={()=>removeItem(item.id)}>Remove</button>
-                  </td>
-                </tr>
+                <><tr>
+                  <td><strong>Album</strong></td>
+                  <td><strong>Artist</strong></td>
+                  <td><strong>Price</strong></td>
+                  <td><strong>Quantity</strong></td>
+                  <td><strong>Total</strong></td>
+                </tr><tr key={index}>
+                    <td>
+                      <img src={item.image} style={{ height: '6rem' }} />
+                    </td>
+                    <td>{item.album}</td>
+                    <td>{item.artist}</td>
+                    <td>${item.price}</td>
+                    <td>Quantity ({item.quantity})</td>
+                    <td>Total: ${item.price * item.quantity}</td>
+                    <td>
+                      <button className='btn btn-info ms-2'
+                        onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>-</button>
+                      <button className='btn btn-info ms-2'
+                        onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>+</button>
+                      <button className='btn btn-danger ms-2'
+                        onClick={() => removeItem(item.id)}>Remove</button>
+                    </td>
+                  </tr></>
                 )
               }
             )
